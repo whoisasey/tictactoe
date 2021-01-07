d8baf58b07f39c9b95f05ac1897b26d656dfa840
atinachang <atinachang@gmail.com>
2021-01-07 18:18:30 -0500

    code cleanup and fb share button

--- app.js
+++ app.js
@@ -41,34 +41,34 @@ const checkGameStatus = ()=> {
 	// is there a winner?
 	if (topLeft && topLeft === topMiddle && topLeft === topRight) {
 		handleWin(topLeft)
-		// cells[0].classList.add('won');
-		// cells[1].classList.add('won');
-		// cells[2].classList.add('won');
+		cells[0].classList.add('won');
+		cells[1].classList.add('won');
+		cells[2].classList.add('won');
 		} else if (middleLeft && middleLeft === middleMiddle && middleLeft === middleRight) {
 			handleWin(middleLeft)
-		//   cells[3].classList.add('won');
-		//   cells[4].classList.add('won');
-		//   cells[5].classList.add('won');
+		  cells[3].classList.add('won');
+		  cells[4].classList.add('won');
+		  cells[5].classList.add('won');
 		} else if (bottomLeft && bottomLeft === bottomMiddle && bottomLeft === bottomRight) {
 			handleWin(bottomRight)
-		//   cells[6].classList.add('won');
-		//   cells[7].classList.add('won');
-		//   cells[8].classList.add('won');
+		  cells[6].classList.add('won');
+		  cells[7].classList.add('won');
+		  cells[8].classList.add('won');
 		} else if (topLeft && topLeft === middleLeft && topLeft === bottomLeft) {
 			handleWin(topLeft)
-		//   cells[0].classList.add('won');
-		//   cells[3].classList.add('won');
-		//   cells[6].classList.add('won');
+		  cells[0].classList.add('won');
+		  cells[3].classList.add('won');
+		  cells[6].classList.add('won');
 		} else if (topMiddle && topMiddle === middleMiddle && topMiddle === bottomMiddle) {
 			handleWin(topMiddle)
-		//   cells[1].classList.add('won');
-		//   cells[4].classList.add('won');
-		//   cells[7].classList.add('won');
+		  cells[1].classList.add('won');
+		  cells[4].classList.add('won');
+		  cells[7].classList.add('won');
 		} else if (topRight && topRight === middleRight && topRight === bottomRight) {
 			handleWin(topRight)
-	//   cells[2].classList.add('won');
-	//   cells[5].classList.add('won');
-	//   cells[8].classList.add('won');
+	  cells[2].classList.add('won');
+	  cells[5].classList.add('won');
+	  cells[8].classList.add('won');
 		} else if( topLeft && topLeft === middleMiddle && topLeft === bottomRight) {
 			handleWin(topLeft)
 		  cells[0].classList.add('won');
@@ -76,16 +76,16 @@ const checkGameStatus = ()=> {
 		  cells[8].classList.add('won');
 		} else if (topRight && topRight === middleMiddle && topRight === bottomLeft) {
 			handleWin(topRight)
-		//   cells[2].classList.add('won');
-		//   cells[4].classList.add('won');
-		//   cells[6].classList.add('won');
+		  cells[2].classList.add('won');
+		  cells[4].classList.add('won');
+		  cells[6].classList.add('won');
 		} else if (topLeft && topMiddle && topRight && middleMiddle && middleLeft && middleRight && bottomLeft && bottomMiddle && bottomRight) {
 			gameIsLive = false;
 			statusDiv.innerHTML = 'No Winner! Game is Tied'
 		} else {
 			xIsNext = !xIsNext;
 			if (xIsNext) {
-				statusDiv.innerHTML = `${xSymbol} is next`
+				statusDiv.innerHTML = `<span>${xSymbol} is next</span>`
 			} else {
 				statusDiv.innerHTML = `<span>${oSymbol} is next</span>`
 			}
@@ -94,20 +94,9 @@ const checkGameStatus = ()=> {
 
 
 // Event Handlers
-const handleReset = () => {
-	xIsNext = true;
-	statusDiv.innerHTML = `${xSymbol} is next`
-	for (const cell of cells) {
-		cell.classList.remove('x')
-		cell.classList.remove('o')
-		cell.classList.remove('won')
-	}
-	gameIsLive = true;
-}
 
 const handleCellClick = e => {
 	const classList = e.target.classList;
-	// console.log(e)
 	if (!gameIsLive || classList[1] === 'x' || classList[1] === 'o'){
 		error.innerHTML = `<span class="error">Error: You've already clicked the cell!!</span>`;
 		return
@@ -120,10 +109,19 @@ const handleCellClick = e => {
 		classList.add('o')
 		checkGameStatus()
 	}
+}
 
-	// if (classList[1] === 'x' || classList[1] === 'o') {
-	// 	console.log(e)
-	// }
+const handleReset = () => {
+	xIsNext = true;
+	statusDiv.innerHTML = `${xSymbol} is next`
+	for (const cell of cells) {
+		cell.classList.remove('x')
+		cell.classList.remove('o')
+		cell.classList.remove('won')
+	}
+	gameIsLive = true;
+	const span = document.querySelector('.error')
+	error.removeChild(span)
 }
 
 // Event Listeners
